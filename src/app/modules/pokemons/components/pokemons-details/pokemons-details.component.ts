72bf18b39817d5912b40bcf65695e4b0e158ce37
Bruno Silva <brunoaugusto.silva@sicoob.com.br>
2024-01-15 03:35:21 -0300

    feature: pokemons details

--- src/app/modules/pokemons/components/pokemons-details/pokemons-details.component.ts
+++ src/app/modules/pokemons/components/pokemons-details/pokemons-details.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-pokemons-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './pokemons-details.component.html',
-  styleUrls: ['./pokemons-details.component.scss']
+  styleUrls: ['./pokemons-details.component.scss'],
 })
-export class PokemonsDetailsComponent {
+export class PokemonsDetailsComponent implements OnInit {
+  private _activatedRoute = inject(ActivatedRoute);
 
+  protected params$ = this._activatedRoute.params;
+
+  ngOnInit(): void {
+    this._activatedRoute.params.forEach(console.log);
+  }
+
+  getPokemonImage(id: string) {
+    return environment.pokeImgs.concat(id as string).concat('.svg');
+  }
 }
--- src/app/modules/pokemons/pokemons.component.ts
+++ src/app/modules/pokemons/pokemons.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pokemons',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './pokemons.component.html',
-  styleUrls: ['./pokemons.component.scss']
+  styleUrls: ['./pokemons.component.scss'],
 })
-export class PokemonsComponent {
-
-}
+export class PokemonsComponent {}
