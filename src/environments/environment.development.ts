12287ef9c2e1743dee44b71732089201d8a5851f
Bruno Silva <bruno.sidf@gmail.com>
2024-01-15 01:53:10 -0300

    env: prod and development

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  pokeapi: 'https://pokeapi.co/api/v2/',
+  pokeImgs:
+    'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  pokeapi: 'https://pokeapi.co/api/v2/',
+  pokeImgs:
+    'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/',
+};
